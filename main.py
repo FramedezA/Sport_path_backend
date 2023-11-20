4d6f62c8a2416ea17f8bf7001577dfde692074a2
StasonShelby <azarovsv2006@gmail.com>
2023-11-20 23:04:06 +0300

    Initional commit for storage branch

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
-import socket, requests
+# This is a sample Python script.
 
-#def get_ip():
-    #response = requests.get('https://api.ipify.org')
-    #if response.status_code == 200:
-        #return response.text
-    #else:
-        #return None
-
-def start_server():
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #server_address = (get_ip(), какой-то порт)
-        server.bind(('localhost', 2000))
-        print('Working...')
-        server.listen(100)
-        while True:
-            client_socket, address = server.accept()
-            data = client_socket.recv(1024).decode('utf-8')
-            HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-            if data == 'basketball':
-                response = 'Введите id басктебольного поля: '
-            elif data == 'football':
-                response = 'Введите id футбольного поля: '
-            elif data == 'volleyball':
-                response = 'Введите id волейбольного поля: '
-            elif data == 'tabletennis':
-                response = 'Введите id теннисного стола: '
-            elif data == 'tennis':
-                response = 'Введите id теннисного корта: '
-            elif data == 'minifootball':
-                response = 'Введите id поля для мини-футбола: '
-            else:
-                response = 'неизвестный запрос'
-
-            client_socket.send(response.encode('utf-8'))
-            client_socket.shutdown(socket.SHUT_WR)
-    except KeyboardInterrupt:
-        server.close()
-        print('End working')
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    start_server()
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
